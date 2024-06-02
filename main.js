ca077425f6931670749406e6d4f2290276e90d9d
zmrabbani <zakimaulana438@gmail.com>
2024-06-02 17:59:18 +0700

    add contact

--- main.js
+++ main.js
@@ -12,3 +12,26 @@ function toggleDetail(e) {
     const detail = $(item).children(".about-exp-item-detail")
     $(detail).slideToggle()
 }
+
+function onFormSubmit(e) {
+    e.preventDefault()
+    const email = $("#inp_email")
+    const subject = $("#inp_subject")
+    const message = $("#inp_message")
+
+    if(!$(email).val()){
+        alert("Email is required")
+    }
+    else if (!$(subject).val()){
+        alert("Subject is required")
+    }
+    else if (!$(message).val()){
+        alert("Message is required")
+    }
+    else {
+        $(email).val("")
+        $(subject).val("")
+        $(message).val("")
+        alert("Form Submitted")
+    }
+}
